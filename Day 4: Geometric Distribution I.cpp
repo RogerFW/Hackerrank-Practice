ba4f562000f05c278fc230d5eb47d0a18c64b283
RogerFW <33913890+RogerFW@users.noreply.github.com>
2019-08-25 21:57:01 +0100

    Create Day 4: Geometric Distribution I.cpp

---
+++ Day 4: Geometric Distribution I.cpp
@@ -0,0 +1,20 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iomanip>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+double geometric(int n, double p) {
+    return pow((1 - p), (n - 1)) * p;
+}
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int n1, n2, n3;
+    cin >> n1 >> n2 >> n3;
+    double p = (double) n1 / n2;
+    
+    cout << fixed << setprecision(3) << geometric(n3, p) << endl;
+    return 0;
+}
