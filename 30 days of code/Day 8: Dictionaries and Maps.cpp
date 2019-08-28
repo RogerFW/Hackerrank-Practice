e179c0d78c43a2189a133f9d42d17635a909eadb
RogerFW <33913890+RogerFW@users.noreply.github.com>
2019-08-28 21:47:01 +0100

    Update Day 8: Dictionaries and Maps.cpp

--- 30 days of code/Day 8: Dictionaries and Maps.cpp
+++ 30 days of code/Day 8: Dictionaries and Maps.cpp
@@ -9,29 +9,24 @@ using namespace std;
 
 int main() {
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */  
-    int N;
-    cin >> N;
-
-    map<string, int> phone_book;
-
-    for (int i = 0; i < N; i++) {
-        string name;
+    int n;
+    string name;
+    long num;
+    cin >> n;
+    cin.ignore();
+    map <string, long> pBook;
+    for (int i = 0; i < n; i++) {
         cin >> name;
-
-        if (!phone_book[name]) {
-            cin >> phone_book[name];
+        cin >> num;
+        pBook[name] = num;
         }
-    }
-
-    for (int i = 0; i < N; i++) {
-        string name;
-        cin >> name;
-
-        if (phone_book[name]) {
-            cout << name << "=" << phone_book[name] << endl;
-        } else {
+    while(cin >> name) {
+        if (pBook.find(name) != pBook.end()) {
+            cout << name << "=" << pBook.find(name)->second << endl;
+        } 
+        else {
             cout << "Not found" << endl;
         }
-    }
+    }    
     return 0;
 }
