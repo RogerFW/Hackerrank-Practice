ccf788beaec7a806979696d24dfc164816b63190
RogerFW <33913890+RogerFW@users.noreply.github.com>
2019-08-29 13:50:21 +0100

    Create itertools.combinations().py

---
+++ Python Practice/itertools.combinations().py
@@ -0,0 +1,6 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+import itertools
+string, combination_size = input().split()
+for length in range(1, int(combination_size) + 1):
+    for combination in itertools.combinations(sorted(string), length):
+        print(''.join(combination))
