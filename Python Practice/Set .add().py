48eb6ce0710c0b8a54eb933e6433cba4dc89e1f5
RogerFW <33913890+RogerFW@users.noreply.github.com>
2019-08-29 11:16:48 +0100

    Create Set .add().py

---
+++ Python Practice/Set .add().py
@@ -0,0 +1,5 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+contry_stamps = set()
+for _ in range(int(input())):
+    contry_stamps.add(input())
+print(len(contry_stamps))
