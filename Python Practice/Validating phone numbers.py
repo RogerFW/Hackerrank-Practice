baa747fa556c27a8ebfff714e2915b4941c3b184
RogerFW <33913890+RogerFW@users.noreply.github.com>
2019-09-12 11:57:39 +0100

    Create Validating phone numbers.py

---
+++ Python Practice/Validating phone numbers.py
@@ -0,0 +1,6 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+import re
+
+for _ in range(int(input())):
+    s = input()
+    print("YES" if re.match(r'^[789]\d{9}$', s) else "NO")
